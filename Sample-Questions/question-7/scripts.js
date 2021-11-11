df836ec417b8c5d5cea79cb4f02923b77fa6156c
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-11-10 17:45:29 -0800

    js updated
    code for question 8 added

--- Sample-Questions/question-7/scripts.js
+++ Sample-Questions/question-7/scripts.js
@@ -17,3 +17,27 @@ function makeCall() {
 }
 
 btnCall.addEventListener("click", makeCall);
+
+
+// let userInp2 = document.querySelector("#userInp2"); 
+let callApi = document.querySelector("#callApi");
+let outputDiv2 = document.querySelector("#outputDiv2"); 
+
+// let url2 = "https://unitube-server.herokuapp.com/playlists"; 
+
+// for error 401, use above url and for 404, the one below
+
+let url2 = "https://jsonplaceholder.typicode.com/dummyUsers"; 
+
+function makeCall() {
+    fetch(url2)
+    .then(response => {
+        if (response.status === 401) {
+            outputDiv2.innerText = 'you are not logged in';
+        } else if ( response.status === 404 ) {
+            outputDiv2.innerText = "404 Page Not Found"
+        }
+    })
+}
+
+callApi.addEventListener("click", makeCall);
