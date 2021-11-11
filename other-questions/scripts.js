db1a0d2e962c4b165eb38f536de30c864d30aa51
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-11-11 10:14:07 -0800

    api question
    Api question added

--- other-questions/scripts.js
+++ other-questions/scripts.js
@@ -17,3 +17,24 @@ for (let i = 0; i<btnText.length; i++) {
         }
     })
 }
+
+let btnCall = document.querySelector("#callApi");
+let outputR = document.querySelector("#outputR"); 
+let url = "https://mystery-api.kushanksriraj.repl.co/get";
+
+
+function clickHandler() {
+    fetch(url)
+    // .then( response => console.log(response))
+    .then(display => {
+        if (display.status== 401  ) {
+            outputR.innerText = "Error 401: your are not logged in";
+        } else if (display.status === 404) {
+            outputR.innerText = "error 404";
+        }
+    })
+}
+
+        
+btnCall.addEventListener("click", clickHandler);
+        
