2af2976c92a91c92b3bb36717dcdd6c5b40a816d
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-11-30 21:21:57 -0800

    Update index.js
    code added

--- oo-prog/index.js
+++ oo-prog/index.js
@@ -1,1 +1,21 @@
 console.log("30 Nov"); 
+const circle = {
+    radius : 1,
+    location : {
+        x:1,
+        y:1
+    },
+    draw : function() {
+        console.log('draw'); 
+    }
+}; 
+
+
+// There are three members in the circle object, they are, radius, location and draw. If a member is a function, we refer it to as a method. Here, draw is a method. Other members are referred to as properties. 
+
+// Properties are used to hold values while functions are defined to use some logic. 
+
+circle.draw(); 
+console.log(circle.location); 
+console.log(circle.radius);
+
