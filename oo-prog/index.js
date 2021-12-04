bfcb892dd642802337d4e6adc6661c9b6becc614
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-12-03 20:46:53 -0800

    Update index.js
    reference types & value types difference

--- oo-prog/index.js
+++ oo-prog/index.js
@@ -45,5 +45,40 @@ function Circle(radius) {
 
 const another = new Circle(4);
 another.draw(); 
+// in the above constructor function, 'this' is basically reference to the object executing this piece of code
 
 // In javaScript, functions are object
+
+// ***********
+// primitives are copied by their values, see below
+
+let number = 10;
+
+function increase(number) {
+    number++;
+    console.log("increased number: " + number); 
+}
+
+increase(number);
+console.log("original number: " + number);
+
+// use object in the above function
+
+let obj = {value: 10}
+function increaseO(obj) {
+    obj.value++; 
+    console.log("increased object: " + obj.value);
+}
+
+increaseO(obj); 
+console.log("original obj: " + obj.value);
+
+/*
+Remember: in JavaScript, we have value types & reference types. 
+Value types are: string, number, boolean, symbol, null, undefined
+Reference types are: objects, functions, arrays. (Functions and arrays are also objects)
+
+Take Away Lesson from the exercise on line 55 to 74:
+Primitives are copied by their value, objects are copied by their reference
+
+*/
