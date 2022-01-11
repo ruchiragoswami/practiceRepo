b69b095e4295738a7b2697ab69ae137e2a97e3ac
ruchiragoswami <ruchigoswami8266@gmail.com>
2022-01-11 12:30:28 -0800

    code added
    in js file

---
+++ Jan_11/jan11.js
@@ -0,0 +1,28 @@
+// console.log('Ruchi, Jan 11') ;
+
+/*
+const tanay = 2
+tanay = 3; 
+// Assignment to a constant variable 
+
+const obj = { a: 1, b: 2 }
+obj.a = 22
+
+
+
+// Will we get an wrror? 
+const obj = { a: 1, b: 2 }
+const obj2 = { a: 3, b: 4 }
+obj = obj2 
+// Yes, assignment to a constat variable
+
+*/ 
+
+// Will we get an error?
+
+// What will it return?
+
+const array = [1, 2, 3, 4]
+array.push(55)
+
+// No error, it will return 5, the length of array. If console logged, it will give the items in the array
