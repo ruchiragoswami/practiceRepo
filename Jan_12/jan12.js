f4a8d6d8c2866e17b8b87c20974342b0f385a8cc
ruchiragoswami <ruchigoswami8266@gmail.com>
2022-01-12 07:03:43 -0800

    comments added
    js file

--- Jan_12/jan12.js
+++ Jan_12/jan12.js
@@ -10,8 +10,16 @@ const mergeArray  = (x, y) =>  { [...x, ...y];
 
 mergeArray(arr1, arr2); 
 
+
 /*
 const toSentenceCase = (str => {
 str.split(' ').map(i) => i[0]
 })
 */ 
+
+
+// const toggleCase = str => str.split("").map((letter, index) => (index % 2) ? letter.toUpperCase() : letter.toLowerCase()).join("")
+// For toggling 👆
+// const toggleCase = str => str.split("").map((letter) => (letter === letter.toLowerCase()) ? letter.toUpperCase() : letter.toLowerCase()).join("");
+// This one sorry👆
+
