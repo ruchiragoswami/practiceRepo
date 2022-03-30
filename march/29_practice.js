14d15376de8ea8073a0e9e83fb017d8e214c73df
ruchiragoswami <ruchigoswami8266@gmail.com>
2022-03-29 20:13:45 -0700

    add: component answer1

--- march/29_practice.js
+++ march/29_practice.js
@@ -19,3 +19,25 @@ export default function App() {
     </div>
   );
 }
+
+//answer1 component
+const numList = [1, 3, 55, 22, 44, 2, 1.5, 1.25, 4.25];
+
+const oddEvenSumReducer = (acc, curr) =>
+  curr % 2 === 0
+    ? { ...acc, even: curr + acc.even }
+    : { ...acc, odd: curr + acc.odd };
+
+const answer1 = numList.reduce(oddEvenSumReducer, { even: 0, odd: 0 });
+
+function Answer1() {
+  return (
+    <section className="answers">
+      <h3> answer 1 </h3>
+      <p> odd: {answer1.odd}</p>
+      <p> even: {answer1.even} </p>
+    </section>
+  );
+}
+
+export { Answer1 };
